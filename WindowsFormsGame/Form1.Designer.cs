60a4c8a5fbd96eec073321df708eed015404b244
Simon Wroblewski <fachinformatiker@kabelmail.de>
2024-02-18 12:28:14 +0100

    Add files via upload

---
+++ WindowsFormsGame/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace WindowsFormsGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelSpielerXGewinne = new System.Windows.Forms.Label();
+            this.labelSpielerOGewinne = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonGegenComputer = new System.Windows.Forms.RadioButton();
+            this.radioButtonGegenSpielerO = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.hilfeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 36);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(78, 38);
+            this.btn1.TabIndex = 0;
+            this.btn1.TabStop = false;
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(93, 36);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(78, 38);
+            this.btn2.TabIndex = 1;
+            this.btn2.TabStop = false;
+            this.btn2.Text = "button2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(174, 36);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(78, 38);
+            this.btn3.TabIndex = 2;
+            this.btn3.TabStop = false;
+            this.btn3.Text = "button3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 83);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(78, 40);
+            this.btn4.TabIndex = 3;
+            this.btn4.TabStop = false;
+            this.btn4.Text = "button4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(93, 83);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(78, 40);
+            this.btn5.TabIndex = 4;
+            this.btn5.TabStop = false;
+            this.btn5.Text = "button5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(174, 83);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(78, 40);
+            this.btn6.TabIndex = 5;
+            this.btn6.TabStop = false;
+            this.btn6.Text = "button6";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(12, 132);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(78, 38);
+            this.btn7.TabIndex = 6;
+            this.btn7.TabStop = false;
+            this.btn7.Text = "button7";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(93, 132);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(78, 38);
+            this.btn8.TabIndex = 7;
+            this.btn8.TabStop = false;
+            this.btn8.Text = "button8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(174, 132);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(78, 38);
+            this.btn9.TabIndex = 8;
+            this.btn9.TabStop = false;
+            this.btn9.Text = "button9";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(269, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.neuToolStripMenuItem,
+            this.hilfeToolStripMenuItem,
+            this.infoToolStripMenuItem,
+            this.beendenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // neuToolStripMenuItem
+            // 
+            this.neuToolStripMenuItem.Name = "neuToolStripMenuItem";
+            this.neuToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.neuToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.neuToolStripMenuItem.Text = "Neu";
+            this.neuToolStripMenuItem.Click += new System.EventHandler(this.neuToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.beendenToolStripMenuItem.Text = "Beenden";
+            this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+            // 
+            // labelSpielerXGewinne
+            // 
+            this.labelSpielerXGewinne.AutoSize = true;
+            this.labelSpielerXGewinne.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSpielerXGewinne.Location = new System.Drawing.Point(6, 20);
+            this.labelSpielerXGewinne.Name = "labelSpielerXGewinne";
+            this.labelSpielerXGewinne.Size = new System.Drawing.Size(41, 13);
+            this.labelSpielerXGewinne.TabIndex = 10;
+            this.labelSpielerXGewinne.Text = "label1";
+            // 
+            // labelSpielerOGewinne
+            // 
+            this.labelSpielerOGewinne.AutoSize = true;
+            this.labelSpielerOGewinne.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSpielerOGewinne.Location = new System.Drawing.Point(6, 40);
+            this.labelSpielerOGewinne.Name = "labelSpielerOGewinne";
+            this.labelSpielerOGewinne.Size = new System.Drawing.Size(41, 13);
+            this.labelSpielerOGewinne.TabIndex = 11;
+            this.labelSpielerOGewinne.Text = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelSpielerXGewinne);
+            this.groupBox1.Controls.Add(this.labelSpielerOGewinne);
+            this.groupBox1.Location = new System.Drawing.Point(12, 192);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(129, 61);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Punktstand:";
+            // 
+            // radioButtonGegenComputer
+            // 
+            this.radioButtonGegenComputer.AutoSize = true;
+            this.radioButtonGegenComputer.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonGegenComputer.Name = "radioButtonGegenComputer";
+            this.radioButtonGegenComputer.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonGegenComputer.TabIndex = 13;
+            this.radioButtonGegenComputer.TabStop = true;
+            this.radioButtonGegenComputer.Text = "Computer";
+            this.radioButtonGegenComputer.UseVisualStyleBackColor = true;
+            this.radioButtonGegenComputer.CheckedChanged += new System.EventHandler(this.radioButtonGegenComputer_CheckedChanged_1);
+            // 
+            // radioButtonGegenSpielerO
+            // 
+            this.radioButtonGegenSpielerO.AutoSize = true;
+            this.radioButtonGegenSpielerO.Location = new System.Drawing.Point(6, 39);
+            this.radioButtonGegenSpielerO.Name = "radioButtonGegenSpielerO";
+            this.radioButtonGegenSpielerO.Size = new System.Drawing.Size(63, 17);
+            this.radioButtonGegenSpielerO.TabIndex = 14;
+            this.radioButtonGegenSpielerO.TabStop = true;
+            this.radioButtonGegenSpielerO.Text = "Mensch";
+            this.radioButtonGegenSpielerO.UseVisualStyleBackColor = true;
+            this.radioButtonGegenSpielerO.CheckedChanged += new System.EventHandler(this.radioButtonGegenSpielerO_CheckedChanged_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonGegenComputer);
+            this.groupBox2.Controls.Add(this.radioButtonGegenSpielerO);
+            this.groupBox2.Location = new System.Drawing.Point(147, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(103, 61);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Spieler O:";
+            // 
+            // hilfeToolStripMenuItem
+            // 
+            this.hilfeToolStripMenuItem.Name = "hilfeToolStripMenuItem";
+            this.hilfeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hilfeToolStripMenuItem.Text = "Hilfe";
+            this.hilfeToolStripMenuItem.Click += new System.EventHandler(this.hilfeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 260);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem neuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.Label labelSpielerXGewinne;
+        private System.Windows.Forms.Label labelSpielerOGewinne;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonGegenComputer;
+        private System.Windows.Forms.RadioButton radioButtonGegenSpielerO;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStripMenuItem hilfeToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsGame/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsGame
+{
+    public partial class Form1 : Form
+    {
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private SoundPlayer soundPlayer; // SoundPlayer-Instanz für das Abspielen des Sounds
+        private bool gegenComputer = false;
+        private int spielerXGewinne = 0;
+        private int spielerOGewinne = 0;
+        private bool xTurn = true; // Variable, um den Spieler zu verfolgen
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGrid();
+            UpdateGewinnLabels();
+            soundPlayer = new SoundPlayer();
+
+            // Zuweisung der SoundPlayer-Instanz zur Soundressource 
+            soundPlayer.Stream = Properties.Resources.fanfare;
+        }
+
+        private void UpdateGewinnLabels()
+        {
+            labelSpielerXGewinne.Text = "Spieler X: " + spielerXGewinne.ToString();
+            labelSpielerOGewinne.Text = "Spieler O: " + spielerOGewinne.ToString();
+        }
+
+        private void radioButtonGegenComputer_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonGegenComputer.Checked)
+            {
+                gegenComputer = true;
+            }
+        }
+
+        private void radioButtonGegenSpielerO_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonGegenSpielerO.Checked)
+            {
+                gegenComputer = false;
+            }
+        }
+
+        private void ComputerMove()
+        {
+            // Überprüfe, ob noch freie Felder auf dem Spielfeld sind
+            if (!AllButtonsDisabled())
+            {
+                // Suche nach einem freien Feld und setze dort das Zeichen des Computers
+                foreach (Control control in Controls)
+                {
+                    if (control is Button)
+                    {
+                        Button button = (Button)control;
+                        if (button.Enabled)
+                        {
+                            button.Text = "O"; // Setze das Zeichen des Computers (O)
+                            button.Enabled = false;
+                            xTurn = true; // Wechsle zum nächsten Spieler (Spieler X)
+                            CheckWinner(); // Überprüfe, ob jemand gewonnen hat
+                            break; // Beende die Schleife nach dem Setzen des Zuges
+                        }
+                    }
+                }
+            }
+        }
+
+        private void InitializeGrid()
+        {
+            // Initialisierung des Gitters
+            foreach (Control control in Controls)
+            {
+                if (control is Button)
+                {
+                    Button button = (Button)control;
+                    button.Enabled = true;
+                    button.Text = "";
+                    button.Click += new EventHandler(Button_Click);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            
+                        Button button = (Button)sender;
+            if (xTurn)
+            {
+                button.Text = "X"; // Setze das Zeichen des Spielers (X)
+            }
+            else
+            {
+                button.Text = "O"; // Setze das Zeichen des Spielers (O)
+            }
+            button.Enabled = false;
+            xTurn = !xTurn; // Wechsle zum nächsten Spieler
+            CheckWinner(); // Überprüfe, ob jemand gewonnen hat
+            if (gegenComputer && !AllButtonsDisabled() && !xTurn)
+            {
+                ComputerMove(); // Lasse den Computer seinen Zug machen, wenn gegen Computer gespielt wird und Spieler O an der Reihe ist
+            }
+
+        }
+
+        private void CheckWinner()
+        {
+            // Überprüfung auf Gewinner oder Unentschieden
+            if (CheckForWin("X"))
+            {
+                PlayWinSound(); // abspielen des Sounds, wenn Spieler X gewinnt
+                MessageBox.Show("X hat gewonnen!","INFO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                spielerXGewinne++;
+                DisableButtons();
+            }
+            else if (CheckForWin("O"))
+            {
+                MessageBox.Show("O hat gewonnen!", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                spielerOGewinne++;
+                DisableButtons();
+            }
+            else if (AllButtonsDisabled())
+            {
+                MessageBox.Show("Unentschieden!", "INFO", MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            }
+
+            UpdateGewinnLabels(); // Aktualisiert die Anzeige der Gewinne
+        }
+
+        private void PlayWinSound()
+        {
+            try
+            {
+                soundPlayer.Play(); // Spielen Sie den Sound ab
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Abspielen des Sounds: " + ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool CheckForWin(string player)
+        {
+            // Überprüfung der Gewinnkombinationen
+            if ((btn1.Text == player && btn2.Text == player && btn3.Text == player) ||
+                (btn4.Text == player && btn5.Text == player && btn6.Text == player) ||
+                (btn7.Text == player && btn8.Text == player && btn9.Text == player) ||
+                (btn1.Text == player && btn4.Text == player && btn7.Text == player) ||
+                (btn2.Text == player && btn5.Text == player && btn8.Text == player) ||
+                (btn3.Text == player && btn6.Text == player && btn9.Text == player) ||
+                (btn1.Text == player && btn5.Text == player && btn9.Text == player) ||
+                (btn3.Text == player && btn5.Text == player && btn7.Text == player))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool AllButtonsDisabled()
+        {
+            // Überprüfung, ob alle Buttons deaktiviert sind (Unentschieden)
+            foreach (Control control in Controls)
+            {
+                if (control is Button && control.Enabled)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void DisableButtons()
+        {
+            // Deaktivierung aller Buttons nach Spielende
+            foreach (Control control in Controls)
+            {
+                if (control is Button)
+                {
+                    control.Enabled = false;
+                }
+            }
+        }    
+
+
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void neuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Alle Buttons zurücksetzen
+            foreach (Control control in Controls)
+            {
+                if (control is Button)
+                {
+                    Button button = (Button)control;
+                    button.Enabled = true;
+                    button.Text = "";
+                }
+            }
+
+            // Das xTurn zurücksetzen, um das nächste Spiel mit "X" zu starten
+            xTurn = true;
+        }
+
+     
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Simon Wroblewski\nFachinformatiker Anwendungsentwicklung", "Über mich...",MessageBoxButtons.OK);
+        }
+
+        private void radioButtonGegenComputer_CheckedChanged_1(object sender, EventArgs e)
+        {
+            gegenComputer = radioButtonGegenComputer.Checked;
+        }
+
+        private void radioButtonGegenSpielerO_CheckedChanged_1(object sender, EventArgs e)
+        {
+            gegenComputer = !radioButtonGegenSpielerO.Checked;
+
+        }
+
+        private void hilfeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hier ist eine grundlegende Spielanleitung:\n" +
+                "1. Das Spielfeld besteht aus einem 3x3-Raster.\n" +
+                "2. Ein Spieler beginnt das Spiel, indem er ein X oder O in ein leeres Feld setzt.\n" +
+                "3. Die Spieler wechseln sich ab, bis das Spielfeld voll ist oder einer der Spieler eine Reihe von drei gleichen Symbolen erreicht.\n" +
+                "4. Ein Spieler gewinnt, wenn er zuerst eine horizontale, vertikale oder diagonale Reihe von drei gleichen Symbolen bildet.\n" +
+                "5. Wenn das Spielfeld voll ist und keiner der Spieler eine Reihe von drei Symbolen hat, endet das Spiel unentschieden.\n" +
+                "6. Die Spieler können versuchen, die Spielzüge ihres Gegners zu blockieren und gleichzeitig eigene Gewinnchancen zu erhöhen.\n" + 
+                "7. Über Radiobuttons erfolgt die Auswahl des Gegners, entweder der Computer(KI) oder ein Mensch.", "Über TicTacToe", MessageBoxButtons.OK);
+        }
+    }
+
+
+}
+
